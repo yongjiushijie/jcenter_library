b93ad2af3f67f68bbda55bcc0fbf6e69fc3e9e6d
wangchao <wangchao@ejiajx.com>
2017-06-20 11:31:23 +0800

    依赖包名

--- app/src/main/java/com/ejjx/versions/MainActivity.java
+++ app/src/main/java/com/ejjx/versions/MainActivity.java
@@ -1,5 +1,50 @@
 package com.ejjx.versions;
 
-public class MainActivity{
+import android.os.Bundle;
 
+import com.ejjx.library.base.BaseActivity;
+import com.ejjx.library.bean.ErrorBean;
+
+
+public class MainActivity extends BaseActivity {
+
+    @Override
+    protected void getExtraEvent(Bundle extras) {
+        super.getExtraEvent(extras);
+    }
+
+    @Override
+    protected int getColor() {
+        return 0;
+    }
+
+    @Override
+    protected boolean isSetSystemBar() {
+        return false;
+    }
+
+    @Override
+    protected boolean isBindEventBus() {
+        return false;
+    }
+
+    @Override
+    protected int contentViewID() {
+        return R.layout.activity_main;
+    }
+
+    @Override
+    protected void initialize() {
+
+    }
+
+    @Override
+    public void onError(ErrorBean errorBean) {
+
+    }
+
+    @Override
+    public void onFailure(String errorMsg) {
+
+    }
 }
